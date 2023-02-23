aa6f6896525ed0052b40aee3a7a8643cc152a2b6
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-02-23 00:26:36 -0400

    Ingresar el precio por item y mostrarlo

--- src/presenter.js
+++ src/presenter.js
@@ -2,15 +2,18 @@ import sumar from "./sumador";
 
 
 const items = document.querySelector("#cantidad_items");
+const precio = document.querySelector("#precio_item");
 const form = document.querySelector("#totalizador-form")
 const div = document.querySelector("#resultado-div")
-
+const precio_resultado = document.querySelector("#precio-span")
+const items_resultado = document.querySelector("#items-span")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // const firstNumber = Number.parseInt(first.value);
 
-  div.innerHTML = items.value;
-
+  //div.innerHTML = items.value;
+  items_resultado.innerHTML = items.value;
+  precio_resultado.innerHTML = precio.value;
 });
