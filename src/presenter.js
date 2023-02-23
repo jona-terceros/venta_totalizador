c3b89f8935dc80946b2b0842d299f1cb440eef55
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-02-23 00:43:42 -0400

    seleccionar estado y  mostrar el porcentaje de impuesto

--- src/presenter.js
+++ src/presenter.js
@@ -9,6 +9,8 @@ const precio_resultado = document.querySelector("#precio-span")
 const items_resultado = document.querySelector("#items-span")
 const estado = document.querySelector("#estado");
 const estado_resul = document.querySelector("#estado-span")
+const estado_impuesto_resul = document.querySelector("#estado-impuesto-span")
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -18,4 +20,24 @@ form.addEventListener("submit", (event) => {
   items_resultado.innerHTML = items.value;
   precio_resultado.innerHTML = precio.value;
   estado_resul.innerHTML = estado.value;
+
+  let impuesto;
+  switch (estado.value) {
+    case "ut":
+      impuesto = 6.65; 
+      break;
+    case "nv":
+      impuesto = 8.00; 
+      break;
+    case "tx":
+      impuesto = 6.25; 
+      break;
+    case "al":
+      impuesto = 4.00; 
+      break;
+    case "ca":
+      impuesto = 8.25; 
+      break;
+  }
+  estado_impuesto_resul.innerHTML = impuesto;
 });
